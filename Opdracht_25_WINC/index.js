f8588e7ddfd5fa556803f7493e6354cdc5fd89d1
AlexanderMetselaar <80575076+AlexanderMetselaar@users.noreply.github.com>
2021-06-23 18:29:16 +0200

    Add files via upload

---
+++ Opdracht_25_WINC/index.js
@@ -0,0 +1,26 @@
+// function exerCise (number1, number2) {
+//     const resultaat1 = Math.pow (number1, 2);
+//     const resultaat2 = Math.pow (number2, 2);
+//     const resultaat3 = number1 + number2;
+//     return(Math.pow(resultaat3, 2));
+// }
+
+// console.log(exerCise(15, 20));
+
+// const Exercise = function (number 1, number2) {
+//     resultaat1 = Math.pow (number1, 2);
+//     resultaat2 = Math.pow (number2, 2);
+//     resultaat3 = number1 + number2;
+//     return(Math.pow(resultaat3, 2));
+// }
+
+// console.log(exerCise(15, 20));
+
+const Exercise = (number1, number2) => {
+    resultaat1 = Math.pow (number1, 2);
+    resultaat2 = Math.pow (number2, 2);
+    resultaat3 = number1 + number2;
+    return(Math.pow(resultaat3, 2));
+}
+
+console.log(Exercise(15, 20));
